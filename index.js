48004600e5b2173720476fd5efa93b48ddce66da
vish9977 <70769498+vish9977@users.noreply.github.com>
2020-12-22 14:59:25 +0530

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+
+        if(this.scrollY>20){
+            $('.navbar').addClass("sticky")
+        }
+        else{
+
+            $('.navbar').removeClass("sticky")
+        
+        }
+    })
+
+
+    // Toggle navbar/menu script
+
+    $('.menu-btn').click(function(){
+
+        $('.navbar .menu').toggleClass('active')
+        $('.menu-btn i').toggleClass('active')
+
+    })
+})
